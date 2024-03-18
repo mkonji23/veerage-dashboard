36d30c1786dc144f725e46cc16ee4c7c78f4bb5a
yoonhong <mkonji23@naver.com>
2024-03-18 15:28:25 +0900

    lint

--- src/pages/Homepage/home.tsx
+++ src/pages/Homepage/home.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client'
 
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Input } from "@/components/Input/input";
-import { Logo } from "@/components/Logo/logo";
-import { Button } from "@/components/Buttons/button";
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { Input } from '@/components/Input/input'
+import { Logo } from '@/components/Logo/logo'
+import { Button } from '@/components/Buttons/button'
 
 const Homepage = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   // initialize states
 
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('')
 
   const [loginData, setLoginData] = useState({
-    email: "",
-    password: "",
-  });
+    email: '',
+    password: '',
+  })
 
   // destructure the values
-  const { email, password } = loginData;
+  const { email, password } = loginData
 
   // handle the input change
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setLoginData((prev) => ({ ...prev, [name]: value }))
   }
   // handle submit here
@@ -32,46 +32,82 @@ const Homepage = () => {
   const handleSubmit = () => {
     try {
       if (!email || !password) {
-        throw new Error("Enter email address and password");
+        throw new Error('Enter email address and password')
       }
 
-      if (email === "user@test.com" && password === "user1234") {
-        router.push("/dashboard");
+      if (email === 'user@test.com' && password === 'user1234') {
+        router.push('/dashboard')
       } else {
-        throw new Error("Invalid email address or password");
+        throw new Error('Invalid email address or password')
       }
     } catch (error: any) {
-      setErrorMessage(error.message);
+      setErrorMessage(error.message)
     }
-  };
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.example.com/data')
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const jsonData = await response.json()
+    } catch (error) {
+      // setError(error.message);
+    } finally {
+      // setLoading(false);
+    }
+  }
 
   return (
     <main className="h-[100dvh] bg-vrBlack w-screen flex flex-col justify-center items-center">
       <Logo />
-
       <div className="mt-6 max-w-[300px]">
-       <p className="text-center text-white">Demo: use <strong>user@test.com</strong> as email address and <strong>user1234</strong> as password</p>
+        <p className="text-center text-white">
+          Demo: use <strong>user@test.com</strong> as email address and <strong>user1234</strong> as password
+        </p>
       </div>
 
       <div className="space-y-3 mt-6">
-        <Input name="email" type="email" value={email} onChange={handleOnChange} />
-        <Input
-          name="password"
-          type="password"
-          value={password}
-          onChange={handleOnChange}
-        />
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+          Email address
+        </label>
 
-        <Button className="bg-vrBlue text-white" onClick={() => handleSubmit()}>
-          Log in
+        <Input name="email" type="email" value={email} onChange={handleOnChange} />
+
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+            Password
+          </label>
+          <div className="text-sm">
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Forgot password?
+            </a>
+          </div>
+        </div>
+
+        <Input name="password" type="password" value={password} onChange={handleOnChange} />
+
+        <Button
+          className="bg-vrBlue text-white"
+          onClick={() => {
+            handleSubmit()
+          }}
+        >
+          Sign in
         </Button>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?
+          <span className="mr-1"></span>
+          <a href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Sign up
+          </a>
+        </p>
       </div>
 
-      {errorMessage && (
-        <p className="error text-red mt-5">🔔 {errorMessage}</p>
-      )}
+      {errorMessage && <p className="error text-red mt-5">🔔 {errorMessage}</p>}
     </main>
-  );
-};
+  )
+}
 
 export default Homepage
